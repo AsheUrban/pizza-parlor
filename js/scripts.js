295d74e729e21ee154ae53cbfa0bd17ea84ae095
Ashe Urban <ashe@goldentongue.com>
2022-06-17 08:44:34 -0700

    ADD function for creating empty pizza object.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic
+
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+let newPizza1 = new Pizza();
+
+Pizza.prototype.AddToppings = function () {
+  
+}
