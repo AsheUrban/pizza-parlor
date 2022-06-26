3ecaef147f176c1182765720de873134033b2313
Ashe Urban <ashe@goldentongue.com>
2022-06-26 15:31:46 -0700

    WIP UI logic & business logic testing. Console isn't working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,51 @@
-// Business Logic for Order
-
-function Order (customers) {
-  this.customers = {};
-  this.currentIdCustomer = 0;
-}
-
-Order.prototype.addCustomer = function(customer) {
-  customer.id = this.assignIdCustomer();
-  this.customers[customer.id] = customer;
-};
-
-Order.prototype.assignIdCustomer = function() {
-  this.currentIdCustomer += 1;
-  return this.currentIdCustomer;
-};
-
-Order.prototype.findCustomer = function(id) {
-  if (this.customers[id] != undefined) {
-    return this.customers[id];
-  }
-  return false;
-}
-
-// Business Logic for Customer
-
-function Customer (firstName) {
-  this.firstName = firstName;
-  this.pizzas = {};
-  this.currentIdPizza = 0;
-}
-
-Customer.prototype.addPizza = function(pizza) {
-  pizza.id = this.assignIdPizza();
-  this.pizzas[pizza.id] = pizza;
-};
-
-Customer.prototype.assignIdPizza = function() {
-  this.currentIdPizza += 1;
-  return this.currentIdPizza;
-};
-
-Customer.prototype.findPizza = function(id) {
-  if (this.pizzas[id] != undefined) {
-    return this.pizzas[id];
-  }
-  return false;
-}
-
 // Business Logic for Pizza
 
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.cost = cost;
+  this.totalCost= 0;
 }
-
-let newPizza = new Pizza();
 
 Pizza.prototype.addCost = function() {
-  if(this.size === "small") {
-    return "$10"
-  } else if(this.size === "Small" && this.toppings === "olives") {
-    return this.cost = "$11" 
-  } else if(this.size === "Small" && this.toppings === "olives" && this.toppings === "pepperoni") {
-    return this.cost = "$12"
+  if(this.size === "Small") {
+    this.totalCost = 10;
   } else if (this.size === "Medium") {
-    return this.cost = "$12"
-  } else if (this.size === "Medium" && this.toppings === "olives") {
-    return this.cost = "$13"
-  } else if (this.size === "Medium" && this.toppings === "olives" && this.toppings === "pepperoni") {
-    return this.cost = "$14"
-  } else if (this.size === "Large") {
-    return this.cost = "$14"
-  } else if (this.size === "Large" && this.toppings === "olives") {
-    return this.cost = "$15"
-  } else if (this.size === "Large" && this.toppings === "olives" && this.toppings === "pepperoni") {
-    return this.cost = "$16"
+    this.totalCost = 12;
+  } else {
+    this.totalCost = 14;
+  }
+  
+
+  if(this.toppings === "olives") {
+    this.totalCost += 1;
+  }
+
+  if(this.toppings === "pepperoni") {
+    this.totalCost += 1;
+  }
+  console.log();
+  return this.totalCost
+ 
 }
-}
+
 // UI Logic
+
+let newPizza = new Pizza ();
+
 
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
     event.preventDefault();
-    newPizza.addCost();
-    const size = $("#size").val();
 
-    
 
-    
-   
+      $("#show-order").html(newPizza.addCost(this.totalCost));
+      console.log();
+     
+      
     });
-  });
+
+    function logReset() {
+    }
+    
+    });
+
