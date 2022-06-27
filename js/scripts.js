2faac65ed2d1e80dba604a1a8037657a903ec264
Ashe Urban <ashe@goldentongue.com>
2022-06-26 22:25:43 -0700

    WIP UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
 // Business Logic for Pizza
 
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.totalCost= 0;
+  console.log(Pizza);
 }
 
+
 Pizza.prototype.addCost = function() {
-  if(this.size === "Small") {
+  if(this.size = "Small") {
     this.totalCost = 10;
-  } else if (this.size === "Medium") {
+  } else if (this.size = "Medium") {
     this.totalCost = 12;
   } else {
     this.totalCost = 14;
-  }
-  
-
-  if(this.toppings === "topping1") {
-    this.totalCost += 1;
+    console.log(size);
   }
 
-  if(this.toppings === "topping2") {
+  if(this.toppings = "topping1") {
     this.totalCost += 1;
+  }
+  if(this.toppings = "topping2") {
+    this.totalCost += 1;
+    console.log(toppings);
   }
 
   return this.totalCost
@@ -32,7 +35,6 @@ Pizza.prototype.addCost = function() {
 
 let newPizza = new Pizza ();
 
-
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
     event.preventDefault();
@@ -41,13 +43,13 @@ $(document).ready(function() {
     const size = $("input:radio[name=size]:checked").val();
    
     let selectedToppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function(element) {
-      selectedToppings.push($(element).val());
-    }); // how does this connect to the toppings in our business logic?
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      selectedToppings.push($(this).val());
+    }); 
 
     // get .addCost
    
-    $("#show-order").html(newPizza.addCost(size, selectedToppings));
+    $("#show-order").html(newPizza.addCost());
     console.log(newPizza);
     });
 
